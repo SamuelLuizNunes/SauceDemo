27b046662e951073de666d7aaa86af0e7f94b860
Samuel Nunes <samuelluiznunes10@gmail.com>
2024-05-07 16:29:20 -0300

    Verificação da página de inventário

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -15,3 +15,8 @@ class TestProdutos():
         self.driver.get(self.url)
         self.driver.find_element(By.ID, 'user-name').send_keys('standard_user')
         self.driver.find_element(By.ID, 'password').send_keys('secret_sauce')
+        self.driver.find_element(By.ID, 'login-button').click()
+
+        assert self.driver.find_element(By.CSS_SELECTOR, '.title').text == 'Products'
+        assert self.driver.find_element(By.ID, 'item_4_title_link').text == 'Sauce Labs Backpack'
+        assert self.driver.find_element(By.CSS_SELECTOR, '.inventory_item:nth-child(1) .inventory_item_price').text == '$29.99'
